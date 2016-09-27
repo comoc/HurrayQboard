c9e083147c29d782bea1102a99d991ed19a1100c
Shigeru Owada <sgrowd@gmail.com>
2016-09-28 02:18:36 +0900

    with RL check using gravity

--- HurrayQboardSTM/HurrayQboardSTM.ino
+++ HurrayQboardSTM/HurrayQboardSTM.ino
@@ -15,10 +15,15 @@ https://github.com/koozyt/qboard_freeimu
 
 // Only two customizable variables
 const int ORIG_FPS = 1000 ;           // loop()が秒間何回呼ばれるか。Q Boardなら1000固定。
-const double LOOP_THR = 80000000 ;    // 繰り返しと判定されるスレッショルド。加速度として得られる値の二乗に比例。
+const double LOOP_THR = 50000000 ;    // 繰り返しと判定されるスレッショルド。加速度として得られる値の二乗に比例。
+const double ROTATE_THR = 1.0 ;    // 回転しているかどうかのスレッショルド。（未確認だが固定でいいのかも？）
 
-const int PROCESS_FPS = 20 ;  // 実際に自己相関計算を行うサンプル単位
-const double WINDOW_HALF_SIZE_IN_SEC = 0.8 ; // 計算を行うウィンドウサイズ。小さい方がきびきび反応するが、遅い周期を検出できない。大きい方が遅い運動を検出できるが、動かし始めてからカウントアップ開始までの時間、止めてからカウント停止までの時間が遅くなる。
+// 実際に自己相関計算を行うサンプル単位
+const int PROCESS_FPS = 20 ;
+
+// 計算を行うウィンドウサイズ。小さい方がきびきび反応するが、遅い周期を検出できない。
+// 大きい方が遅い運動を検出できるが、動かし始めてからカウントアップ開始までの時間、止めてからカウント停止までの時間が遅くなる。
+const double WINDOW_HALF_SIZE_IN_SEC = 0.8 ;
 
 #include <QBoard_FreeIMU.h>
 #include <float.h>
@@ -47,12 +52,8 @@ void setup() {
 
 }
 
-
 double raw_accel[3] ;
 int raw_accel_count = AVERAGE_WINDOW_SIZE ;
-
-
-double accel_diff_log[WINDOW_SIZE][3] ;
 double accel_log[WINDOW_SIZE][3] ;
 int logpos = 0 ;
 
@@ -64,13 +65,11 @@ int mode = 0 ;
 int loopInterv = -1 ;
 int loopInterv_countdown ;
 
-double gravity[3] = {0,0,0} ;
+double gravity[3] = {0,0,0} , gravity_norm[3] ;
 
 void setMode(char inChar) {
   if ( inChar == 's' ) {
     mode = 1 ;
-    for ( int i = 0; i < WINDOW_SIZE; ++i )
-      accel_diff_log[i][0] = accel_diff_log[i][1] = accel_diff_log[i][2] = 0 ;
     logpos = 0 ;
 
     raw_accel[0] = raw_accel[1] = raw_accel[2] = 0 ;
@@ -122,45 +121,59 @@ void loop() {
   raw_accel[1] /= AVERAGE_WINDOW_SIZE ;
   raw_accel[2] /= AVERAGE_WINDOW_SIZE ;
 
-  accel_log[logpos][0] = raw_accel[0] ;
-  accel_log[logpos][1] = raw_accel[1] ;
-  accel_log[logpos][2] = raw_accel[2] ;
+  
+  if( gravity[0] == 0 && gravity[1] == 0 && gravity[2] == 0 ){
+    accel_log[logpos][0] = raw_accel[0] ;
+    accel_log[logpos][1] = raw_accel[1] ;
+    accel_log[logpos][2] = raw_accel[2] ;
+
+    if( logpos == 1 ){ // 一周した. gravityとgravity_normを計算
+      for( int i=0;i<WINDOW_SIZE;++i ){
+        gravity[0] += accel_log[i][0] ;
+        gravity[1] += accel_log[i][1] ;
+        gravity[2] += accel_log[i][2] ;
+        accel_log[i][0] = accel_log[i][1] = accel_log[i][2] = 0 ;
+      }
+      gravity[0] /= WINDOW_SIZE ;
+      gravity[1] /= WINDOW_SIZE ;
+      gravity[2] /= WINDOW_SIZE ;
+
+      Serial.print("G") ;
+      /*Serial.print(gravity[0]);
+      Serial.print(",");
+      Serial.print(gravity[1]);
+      Serial.print(",");
+      Serial.println(gravity[2]);*/
+
+      double glen = sqrt(gravity[0]*gravity[0] + gravity[1]*gravity[1] + gravity[2]*gravity[2] ) ;
+      gravity_norm[0] = gravity[0]/glen ;
+      gravity_norm[1] = gravity[1]/glen ;
+      gravity_norm[2] = gravity[2]/glen ;
+    }
+    logpos = (logpos + (WINDOW_SIZE - 1)) % WINDOW_SIZE ;
+    return ;
+  }
+
+  raw_accel[0] -= gravity[0] ;
+  raw_accel[1] -= gravity[1] ;
+  raw_accel[2] -= gravity[2] ;
+
+  // 重力と垂直な成分のみ用いる
+  accel_log[logpos][0] = raw_accel[1]*gravity_norm[2] - raw_accel[2]*gravity_norm[1] ;
+  accel_log[logpos][1] = raw_accel[2]*gravity_norm[0] - raw_accel[0]*gravity_norm[2] ;
+  accel_log[logpos][2] = raw_accel[0]*gravity_norm[1] - raw_accel[1]*gravity_norm[0] ;
 
   raw_accel[0] = raw_accel[1] = raw_accel[2] = 0 ;
 
-  int prevlogpos = (logpos+1)%WINDOW_SIZE ;
-  accel_diff_log[logpos][0] = accel_log[logpos][0] - accel_log[prevlogpos][0] ;
-  accel_diff_log[logpos][1] = accel_log[logpos][1] - accel_log[prevlogpos][1] ;
-  accel_diff_log[logpos][2] = accel_log[logpos][2] - accel_log[prevlogpos][2] ;
-
-  int i ;
-
-  if( logpos == 1 && gravity[0] == 0 && gravity[1] == 0 && gravity[2] == 0 ){
-    for( i=0;i<WINDOW_SIZE;++i ){
-      gravity[0] += accel_log[i][0] ;
-      gravity[1] += accel_log[i][1] ;
-      gravity[2] += accel_log[i][2] ;
-    }
-    gravity[0] /= WINDOW_SIZE ;
-    gravity[1] /= WINDOW_SIZE ;
-    gravity[2] /= WINDOW_SIZE ;
-    Serial.print(">");
-  }
-
   boolean bIncreasing = false ;
-  double prevCorr = DBL_MAX , prevOri = 0 ;
-  for ( i = MINIMUM_OFFSET; i < WINDOW_SIZE - WINDOW_HALF_SIZE; ++i ) {
-    double corr = 0 , ori = 0 ;
+  double prevCorr = DBL_MAX ;//, prevOri = 0 ;
+  int timediff ;
+  for ( timediff = MINIMUM_OFFSET; timediff < WINDOW_HALF_SIZE; ++timediff ) {
+    double corr = 0 ;
     for ( int j = 0; j < WINDOW_HALF_SIZE; ++j ) {
-      int s1 = (logpos  + j) % WINDOW_SIZE ;
-      int s2 = (logpos + i + j) % WINDOW_SIZE ;
-      corr += (accel_diff_log[s1][0] * accel_diff_log[s2][0] + accel_diff_log[s1][1] * accel_diff_log[s2][1] + accel_diff_log[s1][2] * accel_diff_log[s2][2])/(LOOP_THR*WINDOW_HALF_SIZE) ;
-
-      double cross_product_x = (accel_diff_log[s1][1]*accel_diff_log[s2][2] - accel_diff_log[s1][2]*accel_diff_log[s2][1])/LOOP_THR ;
-      double cross_product_y = (accel_diff_log[s1][2]*accel_diff_log[s2][0] - accel_diff_log[s1][0]*accel_diff_log[s2][2])/LOOP_THR ;
-      double cross_product_z = (accel_diff_log[s1][0]*accel_diff_log[s2][1] - accel_diff_log[s1][1]*accel_diff_log[s2][0])/LOOP_THR ;
-
-      ori += cross_product_x * gravity[0] + cross_product_y * gravity[1] + cross_product_z * gravity[2] ;
+      int s1 = (logpos + j) % WINDOW_SIZE ;
+      int s2 = (logpos + j + timediff) % WINDOW_SIZE ;
+      corr += (accel_log[s1][0] * accel_log[s2][0] + accel_log[s1][1] * accel_log[s2][1] + accel_log[s1][2] * accel_log[s2][2])/(LOOP_THR*WINDOW_HALF_SIZE) ;
     }
 
     if ( !bIncreasing && prevCorr < corr ) bIncreasing = true ;
@@ -168,26 +181,39 @@ void loop() {
       break ;
     }
     prevCorr = corr ;
-    prevOri = ori ;
   }
-  double peak = prevCorr ;
-  double peakori = prevOri ;
 
+  // Increment logpos
   logpos = (logpos + (WINDOW_SIZE - 1)) % WINDOW_SIZE ;
 
-  if ( i == WINDOW_SIZE || peak < 1 ) {
+  double peak = prevCorr ;
+
+  if ( timediff == WINDOW_HALF_SIZE || peak < 1 ) {
     loopInterv = -1 ; // Loop reset
     return ;
   }
 
   // Passed.
   if ( loopInterv == -1 ) {
-    loopInterv = i ;
-    loopInterv_countdown = i ;
-    //Serial.print( "H" ) ;
+    loopInterv = timediff ;
+    loopInterv_countdown = timediff ;
+    //Serial.print( peakori>0 ? "R" : "L" ) ;
   } else if (--loopInterv_countdown == 0 ) {
-    Serial.print( peakori>0 ? "R" : "L" ) ;
-    //Serial.println(peakori) ;
+    // Calc ori
+    double ori_x = 0 , ori_y = 0 , ori_z = 0 , ori = 0 ;
+    for ( int j = 0; j < WINDOW_HALF_SIZE; ++j ) {
+      ori_x = (accel_log[j][1]*accel_log[j+1][2] - accel_log[j][2]*accel_log[j+1][1])/LOOP_THR ;
+      ori_y = (accel_log[j][2]*accel_log[j+1][0] - accel_log[j][0]*accel_log[j+1][2])/LOOP_THR ;
+      ori_z = (accel_log[j][0]*accel_log[j+1][1] - accel_log[j][1]*accel_log[j+1][0])/LOOP_THR ;
+      ori += ori_x * gravity_norm[0] + ori_y * gravity_norm[1] + ori_z * gravity_norm[2] ;
+    }
+
+    if( fabs(ori) > ROTATE_THR ){
+      Serial.print( ori>0 ? "R" : "L" ) ;
+      /*Serial.print(peak) ;
+      Serial.print(":") ;
+      Serial.println(ori) ;*/
+    }
     loopInterv_countdown = loopInterv ;
   }
 }
